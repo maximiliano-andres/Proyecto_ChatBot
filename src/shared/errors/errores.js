d438842ba5941f8d40c3aa8cbec02f45e9e54013
Maximiliano Andres Caniullan Lefiñir <max.andres1995@outlook.com>
2025-03-15 00:10:11 -0300

    errores

--- src/shared/errors/errores.js
+++ src/shared/errors/errores.js
@@ -1,17 +1,23 @@
-
-
 class ErrorHandler {
-    static error404(err, req, res, next) {
-        console.error('Error:', err.message);
-        
-        return res.status(404).render("error404",{
+    // Middleware para manejar rutas no encontradas (Error 404)
+    static error404(req, res, next) {
+        return res.status(404).render("error404", {
             title: "Error 404"
         });
     }
 
+    // Middleware para errores generales de la aplicación
+    static error400(err, req, res, next) {
+        console.error('Error 400:', err.message);
+        return res.status(400).json({ error: 'Solicitud incorrecta' });
+    }
+
+    // Middleware para errores internos del servidor
     static error500(err, req, res, next) {
-        console.error('Error:', err.message);
-        res.status(500).json({ error: 'Error de código interno del servidor' });
+        console.error('Error 500:', err.message);
+        return res.status(404).render("error500", {
+            title: "Error 404"
+        });
     }
 }
 
