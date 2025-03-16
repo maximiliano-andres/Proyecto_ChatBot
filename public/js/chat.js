23ddb37558626c515b81b99c7d45a0c584accd5c
Maximiliano Andres Caniullan Lefiñir <max.andres1995@outlook.com>
2025-03-16 02:08:58 -0300

    chat

---
+++ public/js/chat.js
@@ -0,0 +1,7 @@
+document.getElementById("chat-button").addEventListener("click", function () {
+    document.getElementById("chat-box").style.display = "flex";
+});
+
+document.getElementById("close-chat").addEventListener("click", function () {
+    document.getElementById("chat-box").style.display = "none";
+});
