d63a15ab1da3d72accf4cc5361186662e3f6b208
Maximiliano Andres Caniullan Lefiñir <max.andres1995@outlook.com>
2025-03-18 23:37:39 -0300

    borrado de imagenes

--- src/infrastructure/services/HomeController.js
+++ src/infrastructure/services/HomeController.js
@@ -4,6 +4,10 @@ class HomeController {
             return res.status(200).render('index', { 
                 title: 'Raíz Finanziera',
                 titulo_1: "Bienvenido a Raíz Finanziera",
+                subtitulo:"Seguridad, crecimiento y confianza en cada inversión.",
+                titulo_NH:"Nuestra Historia",
+                texto_NH1:"En Raíz Finanziera, creemos que el éxito financiero se construye sobre bases sólidas de confianza, estrategia y compromiso. Desde nuestra fundación en 2025, hemos trabajado incansablemente para ofrecer soluciones financieras innovadoras, adaptadas a las necesidades de nuestros clientes."
+
                 
             });
         } catch (error) {
